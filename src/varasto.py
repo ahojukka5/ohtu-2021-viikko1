9f8c712f0d3eb4eac13c333dc24ab5c872c5b2b9
Jukka Aho <ahojukka5@gmail.com>
2021-01-25 21:14:43 +0700

    Fix pylint issues in varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -5,7 +5,7 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -31,7 +31,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +40,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
