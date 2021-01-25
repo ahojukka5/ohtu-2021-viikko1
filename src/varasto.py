cac82cd8f73949add21f02d28685d52f41b344bb
Jukka Aho <ahojukka5@gmail.com>
2021-01-25 21:39:57 +0700

    Descease cyclomatic complexity of __init__ of Varasto

--- src/varasto.py
+++ src/varasto.py
@@ -1,10 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(0.0, tilavuus)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
